08ce89213d7ee8dd89d9abb7a25f8db5c8f6aec0
IBrickhouse <mrfurbyb@gmail.com>
2022-06-21 16:59:31 -0700

    Step 2
    Step 2 of tutorial completed

--- game.py
+++ game.py
@@ -3,9 +3,18 @@ screen = pygame.display.set_mode((640,480))
 
 pygame.display.set_caption("Side Scroller")
 
-while True:
-    pygame.display.update()
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.display.quit()
-            exit()
+def menu():
+    image = pygame.image.load('assets/menu.png')
+    image = pygame.transform.scale(image, (640,480))
+    while True:
+        screen.blit(image, (0,0))
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.display.quit()
+                exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.pos[0] in range(300, 325) and event.pos[1] in range(200,228):
+                    print('play')
+
+menu()
