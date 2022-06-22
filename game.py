dec119f63e338ad509266a808b71c3dcb608bbec
IBrickhouse <mrfurbyb@gmail.com>
2022-06-21 17:34:20 -0700

    Step 6 (part a)
    Step 6 (part a) of tutorial completed

--- game.py
+++ game.py
@@ -17,15 +17,17 @@ def menu():
                 if event.pos[0] in range(300, 325) and event.pos[1] in range(200,228):
                     game()
 
-
 def game():
     image = pygame.image.load('assets/level1.png')
     image = pygame.transform.scale(image, (640,480))
     bgx = 0
 
-
     player = pygame.image.load('assets/boy.png')
     player = pygame.transform.rotozoom(player, 0, 0.2)
+    player_y = 325
+    gravity = 1
+    jumpcount = 0
+    jump= 0 
     while True:
         screen.blit(image, (bgx-640, 0))
         screen.blit(image, (bgx, 0))
@@ -35,7 +37,16 @@ def game():
         if bgx <= -640:
             bgx = 0
 
-        screen.blit(player, (50, 325))
+        screen.blit(player, (50, player_y))
+        if player_y < 325:
+            player_y += gravity
+
+        if jump == 1:
+            player_y = player_y - 4
+            jumpcount += 1
+            if jumpcount > 40:
+                 jumpcount = 0
+                 jump=0
 
         pygame.display.update()
         for event in pygame.event.get():
@@ -44,4 +55,5 @@ def game():
                 exit()
             if event.type == pygame.KEYDOWN:
                 print('jump')
+                jump = 1
 menu()
