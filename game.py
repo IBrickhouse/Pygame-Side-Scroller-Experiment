0087c0e7222ad17df49d07be06ded7384fcadf0e
IBrickhouse <mrfurbyb@gmail.com>
2022-06-21 17:02:41 -0700

    Step 3
    Step 3 of tutorial completed

--- game.py
+++ game.py
@@ -15,6 +15,19 @@ def menu():
                 exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.pos[0] in range(300, 325) and event.pos[1] in range(200,228):
-                    print('play')
+                    game()
 
+
+def game():
+    image = pygame.image.load('assets/level1.png')
+    image = pygame.transform.scale(image, (640,480))
+    while True:
+        screen.blit(image, (0,0))
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.display.quit()
+                exit()
+            if event.type == pygame.KEYDOWN:
+                print('jump')
 menu()
